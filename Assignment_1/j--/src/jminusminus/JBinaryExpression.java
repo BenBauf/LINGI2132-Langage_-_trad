c92a1f042695a454f82ed9a9cb13ec637d19dc42
Benoit Baufays <benoit.baufays@conceptbandb.be>
2014-02-02 16:32:36 +0100

    div operator added

--- Assignment_1/j--/src/jminusminus/JBinaryExpression.java
+++ Assignment_1/j--/src/jminusminus/JBinaryExpression.java
@@ -254,3 +254,29 @@ class JMultiplyOp extends JBinaryExpression {
     }
 
 }
+
+/**
+ * The AST node for a division (/) expression.
+ * @author group 20
+ */
+class JDivideOp extends JBinaryExpression {
+	
+	public JDivideOp(int line, JExpression lhs, JExpression rhs) {
+		super(line, "/", lhs, rhs);
+	}
+
+	public JExpression analyze(Context context) {
+		lhs = (JExpression) lhs.analyze(context);
+        rhs = (JExpression) rhs.analyze(context);
+        lhs.type().mustMatchExpected(line(), Type.INT);
+        rhs.type().mustMatchExpected(line(), Type.INT);
+        type = Type.INT;
+        return this;
+	}
+
+	public void codegen(CLEmitter output) {
+		 lhs.codegen(output);
+	     rhs.codegen(output);
+	     output.addNoArgInstruction(IDIV);
+	}
+}
