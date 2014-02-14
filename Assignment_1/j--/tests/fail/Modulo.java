a95d6d59c89de8b71f3e6ac450066028b940489b
ursula <benoit.baufays@conceptbandb.be>
2014-02-14 12:56:18 +0100

    and some other checkstyle

--- Assignment_1/j--/tests/fail/Modulo.java
+++ Assignment_1/j--/tests/fail/Modulo.java
@@ -2,8 +2,8 @@ package fail;
 
 import java.lang.System;
 
-public class Modulo{
-    public static void main(String[] args){
+public class Modulo {
+    public static void main(String[] args) {
         System.out.println('a' % 42);
     }
 }
--- Assignment_1/j--/tests/junit/DivisionTest.java
+++ Assignment_1/j--/tests/junit/DivisionTest.java
@@ -4,21 +4,21 @@ import junit.framework.TestCase;
 import pass.Division;
 
 public class DivisionTest extends TestCase {
-	private Division division;
-	
-	protected void setUp() throws Exception {
-		super.setUp();
-		division = new Division();
-	}
-	
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	
-	public void testDivide() {
-		this.assertEquals(division.divide(0, 42), 0);
-		this.assertEquals(division.divide(42, 1), 42);
-		this.assertEquals(division.divide(127, 3), 42);
-	}
-	
+    private Division division;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.division = new Division();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testDivide() {
+        this.assertEquals(this.division.divide(0, 42), 0);
+        this.assertEquals(this.division.divide(42, 1), 42);
+        this.assertEquals(this.division.divide(127, 3), 42);
+    }
+
 }
