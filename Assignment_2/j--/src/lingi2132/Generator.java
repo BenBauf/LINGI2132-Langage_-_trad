c4aa69b0619719b5b8381658093b130f8239b6a7
Julien Colmonts <julien.colmonts@gmail.com>
2014-03-16 15:48:44 +0100

    First lines for generator

--- Assignment_2/j--/src/lingi2132/Generator.java
+++ Assignment_2/j--/src/lingi2132/Generator.java
@@ -1,5 +1,11 @@
 package lingi2132;
 
+import java.util.ArrayList;
+
+import jminusminus.CLConstants;
+import jminusminus.CLEmitter;
+
+
 
 public class Generator {
 	private String outputDir;
@@ -7,6 +13,30 @@ public class Generator {
 		this.outputDir = outputDir;
 	}
 	public void generateClass() {
+		CLEmitter output = new CLEmitter(true);
+		output.destinationDir(outputDir);
+		ArrayList<String> mods = new ArrayList<>();
+		mods.add("public"); // Add static ? -_-
+		output.addClass(mods , "ClassToGenerate", "java/lang/Object", null, false);
+		
+		output.addMethod(mods, "factorial", "(I)I", null, false);
+		output.addBranchInstruction(CLConstants.IFLE, "ns0");
+		output.addNoArgInstruction(CLConstants.IRETURN);
+		
+		output.addMethod(mods, "maximum", "(I[])I", null, false);
+		output.addNoArgInstruction(CLConstants.IRETURN);
+		
+		output.addMethod(mods, "main", "(S[])", null, false);
+		output.addField(mods, "n", "I", false, 0); // Add Integer.parseInt...
+		output.addField(mods, "a", "I", false, 0);
+		output.addField(mods, "b", "I", false, 0);
+		
+		output.addArrayInstruction(CLConstants.NEWARRAY, "I");
+		
+		
+		output.addNoArgInstruction(CLConstants.RETURN);
+		
+		output.write();
 		
 	}
 	public static void main(String [] args) {
