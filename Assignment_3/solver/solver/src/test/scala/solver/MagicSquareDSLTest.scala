f215aaccca29c1acf93e5a4af13c99e64cf89d4c
ursula <benoit.baufays@conceptbandb.be>
2014-05-05 17:51:47 +0200

    fix julien error

--- Assignment_3/solver/solver/src/test/scala/solver/MagicSquareDSLTest.scala
+++ Assignment_3/solver/solver/src/test/scala/solver/MagicSquareDSLTest.scala
@@ -12,54 +12,43 @@ import solver.expressions.Sum
 class MagicSquareDSLTest extends FlatSpec with Matchers {
 
   "MagicSquare" should "work" in {
-    
+
     val magicNumber = 3 // number of lines/columns
-    
+
     val magicSum = 15
     val zero = 0
     val possibilities = zero.to(magicSum).toArray
-    
-    
-    var s = new SolverDSL(magicNumber^2);
+
+    var s = new SolverDSL(magicNumber * magicNumber);
     s.assigned(i => {
-      "item_" + (i + 1)      
+      "item_" + (i + 1)
     })
-    
-    
-    
+
     s.addConstraint(
-      (s.variable(1)+s.variable(2)+s.variable(3)) === magicSum
-     )
-     
+      (s.variable(0) + s.variable(1) + s.variable(2)) === magicSum)
+
     s.addConstraint(
-      (s.variable(4)+s.variable(5)+s.variable(6)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(7)+s.variable(8)+s.variable(9)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(1)+s.variable(4)+s.variable(7)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(2)+s.variable(5)+s.variable(8)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(3)+s.variable(6)+s.variable(9)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(1)+s.variable(5)+s.variable(9)) === magicSum
-     )
-     
-     s.addConstraint(
-      (s.variable(3)+s.variable(5)+s.variable(7)) === magicSum
-     )
-     
-if (s.solve) println(s.solution)
+      (s.variable(3) + s.variable(4) + s.variable(5)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(6) + s.variable(7) + s.variable(8)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(0) + s.variable(4) + s.variable(8)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(2) + s.variable(4) + s.variable(6)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(0) + s.variable(3) + s.variable(6)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(1) + s.variable(4) + s.variable(7)) === magicSum)
+
+    s.addConstraint(
+      (s.variable(2) + s.variable(5) + s.variable(8)) === magicSum)
+
+    if (s.solve) println(s.solution)
     else println("infeasible")
   }
 
